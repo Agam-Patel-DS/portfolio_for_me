77981419bf08135c6b745c3f6492600cf83df76b
Agam-Patel-DS <colabwithagam@gmail.com>
2024-09-21 15:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ st.set_page_config(layout="wide")
 st.write("##")
 st.subheader("Welcome!")
 st.title("AGAM PATEL")
+st.write("""
+
+""")
+st.write("[https://github.com/Agam-Patel-DS/](Github)")
+st.write("---")
