469e83a29813037bb73292bde7b059b972dbaf96
Agam-Patel-DS <colabwithagam@gmail.com>
2024-09-21 15:59:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import requests
 from streamlit_option_menu import option_menu
-
+from streamlit_lottie import st_lottie
 st.set_page_config(layout="wide")
+
+def load_lottieurl(url):
+  r=requests.get(url)
+  if r.status_code!=200:
+    return None
+  return r.json()
+
+lottie_coder=load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_UBiAADPga8.json")
+
 
 st.write("##")
 st.subheader("Welcome!")
@@ -19,3 +29,14 @@ with st.container():
     icons=['person','code-slash','chat-left-text-fill'],
     orientation='horizontal'
   )
+
+if selected=='About':
+  with st.container():
+    col1,col2=st.columns(2)
+    with col1:
+      st.write("##")
+      st.title("I'm Agam Patel")
+      st.subheader("Undergrad at Sri Ram Institute of Technology")
+    with col2:
+      st_lottie(lottie_coder)
+  st.write("---")
